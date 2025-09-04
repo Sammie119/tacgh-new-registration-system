9cce6d934069d201e0e5cb6c37148b8e8d1faecf
Samuel Sarpong-Duah <sammav2018@gmail.com>
2025-09-04 15:26:56 +0000

    Favor Icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TAC-GH Registration | Welcome</title>
-    <link rel="shortcut icon" href="{{ asset('dist/assets/img/favicon.ico') }}" type="image/ico">
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/ico">
 
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.css') }}"><!--end::Required Plugin(AdminLTE)--><!-- apexcharts -->
 </head>
